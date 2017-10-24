645bfecfd4ab3164bf5090bb783294032272c5e2
manucarbonell@github.com <manucarbonell@github.com>
2017-10-24 16:38:02 +0200

    Train with all training set examples

--- ctc_tensorflow_example.py
+++ ctc_tensorflow_example.py
@@ -22,7 +22,7 @@ from utils import maybe_download as maybe_download
 from utils import sparse_tuple_from as sparse_tuple_from
 
 iam_train=IAM_input()
-X,Y=iam_train.get_batch()
+
 
 # Constants
 SPACE_TOKEN = '<space>'
@@ -31,7 +31,6 @@ FIRST_INDEX = ord('a') - 1  # 0 is reserved to space
 
 # Some configs
 ################################################### CHANGE NUMBER OF FEATURES TO IMAGE HEIGHT
-#num_features = 13
 num_features=64
 # Accounting the 0th indice +  space + blank label = 28 characters
 num_classes = ord('z') - ord('a') + 1 + 1 + 1
@@ -45,33 +44,8 @@ n_channels = 1
 initial_learning_rate = 1e-2
 momentum = 0.9
 
-num_examples = 1
+num_examples = 50
 num_batches_per_epoch = int(num_examples/batch_size)
-
-# Loading the data
-
-target_filename = maybe_download('LDC93S1.txt', 62)
-
-train_inputs=X
-#train_inputs=np.transpose(train_inputs)
-print("TRAIN INPUTS AFTER",train_inputs.shape, train_inputs)
-###############################
-
-train_seq_len = [train_inputs.shape[1]]
-print (train_seq_len,"TRAIN SEQ LEN")
-
-# Readings targets
-
-targets=Y[0]
-
-####################################################
-
-# Creating sparse representation to feed the placeholder
-train_targets = sparse_tuple_from([targets])
-
-# We don't have a validation dataset :(
-val_inputs, val_targets, val_seq_len = train_inputs, train_targets, \
-                                       train_seq_len
 
 
 # THE MAIN CODE!
@@ -164,8 +138,15 @@ with tf.Session(graph=graph) as session:
         start = time.time()
 
         for batch in range(num_batches_per_epoch):
+            print("EPOCH STEP",batch)
 
-            feed = {inputs: train_inputs,
+            X, Y = iam_train.get_batch()
+
+            train_seq_len = [X.shape[1]]
+            Y = Y[0]
+
+            train_targets = sparse_tuple_from([Y])
+            feed = {inputs: X,
                     targets: train_targets,
                     seq_len: train_seq_len}
 
@@ -176,17 +157,13 @@ with tf.Session(graph=graph) as session:
         train_cost /= num_examples
         train_ler /= num_examples
 
-        val_feed = {inputs: val_inputs,
-                    targets: val_targets,
-                    seq_len: val_seq_len}
 
-        val_cost, val_ler = session.run([cost, ler], feed_dict=val_feed)
 
-        log = "Epoch {}/{}, train_cost = {:.3f}, train_ler = {:.3f}, val_cost = {:.3f}, val_ler = {:.3f}, time = {:.3f}"
+        log = "Epoch {}/{}, train_cost = {:.3f}, train_ler = {:.3f} time = {:.3f}"
         print(log.format(curr_epoch+1, num_epochs, train_cost, train_ler,
-                         val_cost, val_ler, time.time() - start))
+                         time.time() - start))
 
-        
+
     # Decoding
     d = session.run(decoded[0], feed_dict=feed)
     str_decoded = ''.join([chr(x) for x in np.asarray(d[1]) + FIRST_INDEX])
